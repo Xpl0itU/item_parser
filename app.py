c610424143b27c30d69f7d614f048e265d0415fc
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2024-01-18 15:50:35 +0100

    Move unused file in test/data to the root

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 if __name__ == "__main__":
     from item_parser.GRParser import GRParser
-    import os.path
     from pprint import pprint
 
-    with open(
-        os.path.join("test", "data", "stdout_bug_conjured.gr"), encoding="utf-8"
-    ) as f:
+    with open("stdout_bug_conjured.gr", encoding="utf-8") as f:
         pprint(GRParser(f.read()).parse_string())
