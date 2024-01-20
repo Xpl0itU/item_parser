c97e11050ef44fa79e73c5fe770b218d7da8e8db
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2024-01-20 12:05:28 +0100

    Refactor Item class to use dataclass

--- item_parser/Item.py
+++ item_parser/Item.py
@@ -1,27 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Item:
-    def __init__(self, name, sell_in, quality):
-        self._name = name
-        self._sell_in = sell_in
-        self._quality = quality
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def sell_in(self):
-        return self._sell_in
-
-    @property
-    def quality(self):
-        return self._quality
-
-    def __eq__(self, other):
-        return (
-            self.name == other.name
-            and self.sell_in == other.sell_in
-            and self.quality == other.quality
-        )
-
-    def __repr__(self):
-        return f"Item(name={self.name}, sell_in={self.sell_in}, quality={self.quality})"
+    name: str
+    sell_in: int
+    quality: int
