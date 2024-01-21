745622e0b5f02c66638b8cccbbd32aff6a7798f7
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2024-01-21 19:58:46 +0100

    Add tests for ParsedGRDataFormatter

--- item_parser/__init__.py
+++ item_parser/__init__.py
@@ -1,3 +1,4 @@
 from item_parser import GRParser
 from item_parser import Items
 from item_parser import Hooks
+from item_parser import ParsedGRDataFormatter
---
+++ test/test_gr_formatter.py
@@ -0,0 +1,28 @@
+from item_parser.Items import Item
+from item_parser.ParsedGRDataFormatter import ParsedGRDataFormatter
+import os
+
+
+def test_format_parsed_data():
+    parsed_data = {1: [Item("item1", 10, 5), Item("item2", 5, 10)]}
+    formatter = ParsedGRDataFormatter(parsed_data)
+    expected_output = (
+        "-------- day 1 --------\nname, sellIn, quality\nitem1, 10, 5\nitem2, 5, 10\n"
+    )
+    assert str(formatter) == expected_output
+
+
+def test_empty_parsed_data():
+    parsed_data = {}
+    formatter = ParsedGRDataFormatter(parsed_data)
+    expected_output = ""
+    assert str(formatter) == expected_output
+
+
+def test_export_to_file():
+    parsed_data = {1: [Item("item1", 10, 5), Item("item2", 5, 10)]}
+    formatter = ParsedGRDataFormatter(parsed_data)
+    formatter.export_to_file("test.txt")
+    with open("test.txt", "r", encoding="utf-8") as file:
+        assert file.read() == str(formatter)
+    os.remove("test.txt")
