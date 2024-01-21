c64014659f27f2eaa4178695709138cc9dc556d4
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2024-01-21 11:17:13 +0100

    Simplify item creation in Hooks

--- item_parser/Hooks.py
+++ item_parser/Hooks.py
@@ -3,6 +3,7 @@ from item_parser.Items import NormalItem, ConjuredItem, AgedBrie, Sulfuras, Back
 
 class Hook:
     priority = -1
+    base_class = None
 
     def __init__(self, raw_item_data):
         self.raw_item_data = raw_item_data
@@ -11,78 +12,56 @@ class Hook:
     def get_priority(cls):
         return cls.priority
 
+    @classmethod
+    def __create_item_from_raw_data(cls, data):
+        return cls.base_class(
+            name=data["name"],
+            sell_in=int(data["sellIn"]),
+            quality=int(data["quality"]),
+        )
+
     def can_hook(self):
         pass
 
     def hook(self):
-        pass
+        return self.__create_item_from_raw_data(self.raw_item_data)
 
 
 class NormalItemHook(Hook):
     priority = 1
+    base_class = NormalItem
 
     def can_hook(self):
         return True
 
-    def hook(self):
-        return NormalItem(
-            name=self.raw_item_data["name"],
-            sell_in=self.raw_item_data["sellIn"],
-            quality=self.raw_item_data["quality"],
-        )
-
 
 class ConjuredItemHook(Hook):
     priority = 2
+    base_class = ConjuredItem
 
     def can_hook(self):
         return self.raw_item_data["name"].startswith("Conjured")
 
-    def hook(self):
-        return ConjuredItem(
-            name=self.raw_item_data["name"],
-            sell_in=self.raw_item_data["sellIn"],
-            quality=self.raw_item_data["quality"],
-        )
-
 
 class AgedBrieHook(Hook):
     priority = 2
+    base_class = AgedBrie
 
     def can_hook(self):
         return self.raw_item_data["name"].startswith("Aged Brie")
 
-    def hook(self):
-        return AgedBrie(
-            name=self.raw_item_data["name"],
-            sell_in=self.raw_item_data["sellIn"],
-            quality=self.raw_item_data["quality"],
-        )
-
 
 class SulfurasHook(Hook):
     priority = 2
+    base_class = Sulfuras
 
     def can_hook(self):
         return self.raw_item_data["name"].startswith("Sulfuras")
 
-    def hook(self):
-        return Sulfuras(
-            name=self.raw_item_data["name"],
-            sell_in=self.raw_item_data["sellIn"],
-            quality=self.raw_item_data["quality"],
-        )
-
 
 class BackstageHook(Hook):
     priority = 2
+    base_class = Backstage
 
     def can_hook(self):
         return self.raw_item_data["name"].startswith("Backstage passes")
-
-    def hook(self):
-        return Backstage(
-            name=self.raw_item_data["name"],
-            sell_in=self.raw_item_data["sellIn"],
-            quality=self.raw_item_data["quality"],
-        )
